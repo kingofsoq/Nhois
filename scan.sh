9649f52ad293ca67670a472a59fef90ab1c57a36
kingofsoq <kingofsoq@gmail.com>
2019-11-05 01:37:03 +0900

    Update scan.sh
    Remove some space and translate in to english

--- scan.sh
+++ scan.sh
@@ -1,35 +1,31 @@
 #!/bin/bash 
 # ================//===============
 # Wednesday, 22 May 2019 @10:00 PM
+# Tuesday, 5 November 2019 @1:35 AM
 # ================//===============
-# script sederhana untuk 2 command sekaligus.
-# cocok untuk ngumpulin informasi ala-ala hekel inspect element.
-# hahaha, bercana kok! semoga bermanfaat.
+# simple script for running nmap & whois
 # =================================
-# ps: ini berantakan bgt.. sangat boleh kok diubek-ubek 
-# asalkan namaku masih ada disini :)
+# ps: still mess, need modification 
+# you free to modified, just dont remove my name
 
 while getopts i: option 
 do 
  case "${option}" 
- in 
+  in 
  i) IP=${OPTARG};; 
- esac
-
+  esac
 printf "**************************************************************\n"
-echo "MEMULAI SCAN NMAP DI IP: "$IP
+echo "STARTING NMAP WITH IP: "$IP
 printf "**************************************************************\n"
 bash -c "nmap -F -T4 $IP"
 
-
 printf "\n\n\n\n\n\n**************************************************************\n"
-echo "MEMULAI MENJALANKAN WHOIS DI IP: "$IP
+echo "STARTING WHOIS WITH IP: "$IP
 printf "**************************************************************\n"
 bash -c "whois $IP"
 printf "\n\n\n**********************************************\n"
 
-
-printf "** SCRIPT MADE BY @kingofsoq\n"
+printf "** SCRIPT MADE BY @kingofsoq with LOVE\n"
 echo "**(G) http://github.com/kingofsoq"
 echo "**(I) http://instagram.com/kingofsoq"
 echo "**(T) http://twitter.com/kingofsoq"
